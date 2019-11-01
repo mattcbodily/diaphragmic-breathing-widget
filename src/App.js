dbf9f05abfcb27e6e4ef454a273723bdf1c53efd
Matthew Bodily <mattcbodily@gmail.com>
2019-11-01 16:53:36 -0600

    added input to landing component preparing for repetition input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Landing from './Components/Landing';
 import Breathe from './Components/Breathe';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Landing />
       <Breathe />
     </div>
   );
--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Landing = () => {
-    return(
-        <div>Hawlooo</div>
-    )
+class Landing extends Component {
+    constructor(){
+        super();
+        this.state = {
+            repInput: ''
+        }
+    }
+
+    handleRepInput(val){
+        this.setState({
+            repInput: val
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <input 
+                    value={this.state.repInput}
+                    onChange={(e) => this.handleRepInput(e.target.value)}/>
+            </div>
+        )
+    }
 }
 
 export default Landing;
