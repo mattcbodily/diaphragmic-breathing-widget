4a7024bf270448c9e89f639463ea3b72c674b2cb
Matthew Bodily <mattcbodily@gmail.com>
2019-11-05 16:05:04 -0700

    adding styling

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -35,11 +35,12 @@ class Landing extends Component {
         return(
             <div id='landing-container'>
                 {!breatheToggle
-                ?(<section>
+                ?(<section id='input-container'>
                     <input
                         type='number' 
                         value={repInput}
                         onChange={(e) => this.handleRepInput(e.target.value)}/>
+                    <br/>
                     <button onClick={this.handleToggle}>Get Started</button>
                   </section>)
                 : (<Breathe 
